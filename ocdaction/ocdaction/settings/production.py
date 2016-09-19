a375905d7a531c711c8f34e4d0afdbeb1190305d
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-09-19 23:37:41 +0100

    Added grunt and frontent build pipeline

---
+++ ocdaction/ocdaction/settings/production.py
@@ -0,0 +1,52 @@
+from ocdaction.settings import *
+import dj_database_url
+
+DEBUG = False
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+ALLOWED_HOSTS = ['staging-ocdaction.herokuapp.com']
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+        },
+        'fashrevwall': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
+
