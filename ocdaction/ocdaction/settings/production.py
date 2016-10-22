0b08113471ee7da3c42bdfcc1b92e009d617e33f
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-10-22 21:32:33 +0100

    Added debug true to production settings temporarily

--- ocdaction/ocdaction/settings/production.py
+++ ocdaction/ocdaction/settings/production.py
@@ -1,7 +1,7 @@
 from ocdaction.settings import *
 import dj_database_url
 
-DEBUG = False
+DEBUG = True
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'] = dj_database_url.config()
