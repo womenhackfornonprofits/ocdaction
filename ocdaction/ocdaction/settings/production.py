94b6bbddd52fb5069fe33a2ab8a3821ff8226ced
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-10-22 20:00:00 +0100

    Changed debug to true to test for a static files issue

--- ocdaction/ocdaction/settings/production.py
+++ ocdaction/ocdaction/settings/production.py
@@ -1,10 +1,10 @@
 from ocdaction.settings import *
 import dj_database_url
 
-DEBUG = False
+DEBUG = True
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
