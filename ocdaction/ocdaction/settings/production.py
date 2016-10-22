21ee23d60df0eba826fd3b55ab1f0492f111b24f
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-10-22 19:32:47 +0100

    Tweaks to remove some fonts

--- ocdaction/ocdaction/settings/production.py
+++ ocdaction/ocdaction/settings/production.py
@@ -4,7 +4,7 @@ import dj_database_url
 DEBUG = False
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
