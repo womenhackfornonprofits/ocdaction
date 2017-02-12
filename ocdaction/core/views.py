744043bc0064eadec1f1a645b29e6bc87f6d3110
lmash <lauramachet@Lauras-MBP.home>
2017-02-12 22:30:52 +0000

    Use request.user directly in task_list

--- ocdaction/core/views.py
+++ ocdaction/core/views.py
@@ -33,11 +33,7 @@ def ActView(request):
     The Act View.
     """
     template_name = "core/act.html"
-
-    # Get the logged in user, then retrieve the users unarchived tasks
-    instance = request.user
-    task_list = Task.objects.filter(user=instance.id, is_archived=False)
-
+    task_list = Task.objects.filter(user=request.user, is_archived=False)
     return render(request, template_name, {'tasks': task_list})
 
 
