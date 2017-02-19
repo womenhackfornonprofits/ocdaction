1a5453ee8670dd638397d604652bd6fd5a082124
Lili <kastilio.liliana@gmail.com>
2017-02-19 19:02:05 +0000

    Correcting static path

--- ocdaction/ocdaction/settings/__init__.py
+++ ocdaction/ocdaction/settings/__init__.py
@@ -134,13 +134,13 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 #
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'ocdaction/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "ocdaction/static"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 
