169ab248fa875e1074db2fe2a9cb328adeefbe83
Lili <kastilio.liliana@gmail.com>
2017-02-19 19:44:36 +0000

    Allow for fields to be null

--- ocdaction/profiles/models.py
+++ ocdaction/profiles/models.py
@@ -7,8 +7,8 @@ class OCDActionUser(AbstractEmailUser):
     """ User is authenticated with their email
     but for app purposes users also have usernames.
     """
-    username = models.CharField(max_length=24)
-    date_birth = models.DateField('date of birth')
+    username = models.CharField(max_length=24, null=True)
+    date_birth = models.DateField('date of birth', null=True)
     have_ocd = models.BooleanField(default=False)
 
     def __str__(self):
