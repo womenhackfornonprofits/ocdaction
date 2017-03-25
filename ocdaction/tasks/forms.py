b2812ed422d3a3c419fd57f91a98faaf67283eea
natalia-z <nataliazdeb@gmail.com>
2017-03-25 13:12:30 +0000

    created edit task form and eadit task template

--- ocdaction/tasks/forms.py
+++ ocdaction/tasks/forms.py
@@ -8,3 +8,9 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ('task_name', 'task_fears', 'task_compulsions', 'task_goals')
+
+class EditTaskForm(forms.ModelForm):
+
+    class Meta:
+        model = Task
+        fields = ('task_name')
