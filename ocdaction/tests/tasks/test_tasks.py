4da8be97f14e595bcb74b3b3728d316bc439a272
laura <Masher@iMac.home>
2017-04-06 20:42:11 +0100

    Manual Add task and check added

---
+++ ocdaction/tests/tasks/test_tasks.py
@@ -0,0 +1,56 @@
+import pytest
+import datetime
+import factory
+from pytest_factoryboy import register
+
+from profiles.models import OCDActionUser
+from tasks.models import Task
+
+# use the below if multiple DB access tests
+#pytestmark = pytest.mark.django_db
+
+@pytest.mark.django_db
+def test_addtask():
+    testuser = OCDActionUser(username='autouser',
+                         date_birth=datetime.date(2000, 1, 2),
+                         have_ocd=True,
+                         email='autouser@yopmail.com'
+                         )
+    testuser.save()
+
+    # Check there are 0 tasks before a new task is added
+    number_tasks = Task.objects.filter(user_id=testuser.id).count()
+    assert number_tasks == 0
+
+    task = Task(task_name='taskname',
+                is_archived=False,
+                task_fears='fears',
+                task_compulsions='compulsions',
+                task_goals='goals',
+                user_id=testuser.id
+                )
+
+    task.save()
+
+    # Check there is 1 task after a new task is added
+    number_tasks = Task.objects.filter(user_id=testuser.id).count()
+    assert number_tasks == 1
+
+    # Teardown
+    OCDActionUser.objects.get(username='autouser').delete()
+
+
+# class UserFactory(factory.Factory):
+#
+#     class Meta:
+#         model = OCDActionUser
+#
+# register(UserFactory)
+#
+# @pytest.mark.django_db
+# def test_addtaskUsefactory(user_factory):
+#     testuser = user_factory(username='autouser',
+#                          date_birth=datetime.date(2000, 1, 2),
+#                          have_ocd=True,
+#                          email='autouser@yopmail.com'
+#                             )
