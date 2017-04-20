b2fafa732d0a762bc711b897798b325754a787af
Lili <kastilio.liliana@gmail.com>
2017-04-20 22:33:37 +0100

    Tests config

---
+++ ocdaction/ocdaction/settings/test.py
@@ -0,0 +1,21 @@
+from ocdaction.settings import *
+
+import django
+
+
+class DisableMigrations(object):
+
+    def __init__(self):
+        self._django_version = django.VERSION
+
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        if self._django_version >= (1, 9):
+            return None
+        else:
+            return 'notmigrations'
+
+
+MIGRATION_MODULES = DisableMigrations()
