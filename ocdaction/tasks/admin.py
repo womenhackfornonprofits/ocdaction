d46e8a84958a234618639e6013fb22f9f1f7cc2a
Ruta <ruta.misutyte@gmail.com>
2017-04-26 22:03:59 +0100

    Removed AnxietyScore model and added score choices to AnxietyScoreCard model

--- ocdaction/tasks/admin.py
+++ ocdaction/tasks/admin.py
@@ -1,17 +1,11 @@
 from django.contrib import admin
-from .models import Task, AnxietyScore, AnxietyScoreCard
+from .models import Task, AnxietyScoreCard
 
 
 class TaskAdmin(admin.ModelAdmin):
     list_select_related = True
     list_display = ('task_name', 'user')
     search_fields = ['task_name']
-
-
-class AnxietyScoreAdmin(admin.ModelAdmin):
-    list_select_related = True
-    list_display = ('user_name', 'task', 'score')
-    search_fields = ['task__task_name']
 
 
 class AnxietyScoreCardAdmin(admin.ModelAdmin):
@@ -31,5 +25,4 @@ class AnxietyScoreCardAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Task, TaskAdmin)
-admin.site.register(AnxietyScore, AnxietyScoreAdmin)
 admin.site.register(AnxietyScoreCard, AnxietyScoreCardAdmin)
