c6b0ce1a7be815ad37cdef62d36401530409d8c5
laura <Masher@iMac.home>
2017-07-28 13:52:44 +0100

    Home page displays dashboard when logged in

--- ocdaction/core/views.py
+++ ocdaction/core/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render, redirect
 
 
+def home_index(request):
+    """
+    The Home Index view.
+    """
+
+    template_name = "index.html"
+    if request.user.is_authenticated():
+        return redirect('dashboard-index')
+    else:
+        return render(request, template_name)
+
+
 def dashboard_index(request):
     """
     The Dashboard Index view.
