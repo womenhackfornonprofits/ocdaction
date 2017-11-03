decf03f88a0347aa7f4d5257b97e4e7022416589
Paula Stepinska <paula.stepinska@gmail.com>
2017-11-03 14:59:37 +0000

    Remove dashboard urls and create tasks specific urls instead

--- ocdaction/tasks/urls.py
+++ ocdaction/tasks/urls.py
@@ -1,9 +1,9 @@
 """ocdaction URL Configuration
-    main urls file, it imports all the app urls separately for tidiness.
+    Tasks specific urls for tasks workflows
 """
 from django.conf.urls import url
 
-from tasks.views import (
+from .views import (
     task_list,
     task_add,
     task_edit,
@@ -15,42 +15,42 @@ from tasks.views import (
 
 urlpatterns = [
     url(
-        r'^tasks/$',
+        r'^$',
         task_list,
         name="task-list"
     ),
     url(
-        r'^tasks/(?P<archived>)archived/$',
+        r'^(?P<archived>)archived/$',
         task_list,
         name="task-list-archived"
     ),
     url(
-        r'^tasks/new/$',
+        r'^new/$',
         task_add,
         name="task-add"
     ),
     url(
-        r'^tasks/(?P<task_id>\d+)/edit$',
+        r'^(?P<task_id>\d+)/edit$',
         task_edit,
         name="task-edit"
     ),
     url(
-        r'^tasks/(?P<task_id>\d+)/archive/$',
+        r'^(?P<task_id>\d+)/archive/$',
         task_archive,
         name="task-archive"
     ),
     url(
-        r'^tasks/(?P<task_id>\d+)/$',
+        r'^(?P<task_id>\d+)/$',
         task_score_form,
         name="task-score-form"
     ),
     url(
-        r'^tasks/(?P<task_id>\d+)/complete/(?P<score_id>\d+)/$',
+        r'^(?P<task_id>\d+)/complete/(?P<score_id>\d+)/$',
         task_complete,
         name="task-complete"
     ),
     url(
-        r'^tasks/(?P<task_id>\d+)/summary/(?P<score_id>\d+)/$',
+        r'^(?P<task_id>\d+)/summary/(?P<score_id>\d+)/$',
         task_summary,
         name="task-summary"
     ),
