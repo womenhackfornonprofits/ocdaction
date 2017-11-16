54722abba815509be2e26d25c70b473ce02ea716
Carly Amos-Jones <carly_amos@yahoo.co.uk>
2017-11-16 20:16:28 +0000

    Moved logic for displaying "-" for no anxiety level into template

--- ocdaction/challenges/models.py
+++ ocdaction/challenges/models.py
@@ -44,15 +44,6 @@ class Challenge(models.Model):
             latest_initial_anxiety_level = -1
         return latest_initial_anxiety_level
 
-    def get_latest_initial_anxiety_level_as_str(self):
-        try:
-            anxiety_score_card = AnxietyScoreCard.objects.filter(challenge=self).last()
-            latest_initial_anxiety_level = anxiety_score_card.anxiety_at_0_min
-        except:
-            AnxietyScoreCard.DoesNotExist
-            latest_initial_anxiety_level = "-"
-        return latest_initial_anxiety_level
-
 class AnxietyScoreCard(models.Model):
     """
     Anxiety score card is a collection of scores for the challenge
