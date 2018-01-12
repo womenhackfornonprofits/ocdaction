5278836857bd98f6ccb6d830d8b74372eb2a9eb6
Carly Amos-Jones <carly_amos@yahoo.co.uk>
2018-01-12 14:11:02 +0000

    Initial test for profiles

--- ocdaction/tests/profiles/test_profiles.py
+++ ocdaction/tests/profiles/test_profiles.py
@@ -2,10 +2,6 @@ import pytest
 
 from tests.factories import UserFactory
 from profiles.models import OCDActionUser
-from django.test import TestCase, RequestFactory
-from profiles.views import *
-from profiles.forms import *
-from django.core.urlresolvers import reverse
 
 @pytest.mark.django_db
 def test_create_user():
@@ -18,3 +14,4 @@ def test_create_user():
     UserFactory.create()
     number_users = OCDActionUser.objects.count()
     assert number_users == 1
+
