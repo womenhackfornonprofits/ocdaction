5f257ef7be480e6a8f5f5e9182de47c0e8a60a7b
Carly Amos-Jones <carly_amos@yahoo.co.uk>
2018-04-26 19:31:09 +0100

    Logout user before deleting account

--- ocdaction/profiles/views.py
+++ ocdaction/profiles/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from registration.backends.default import views as registration_views
 from profiles.forms import OCDActionUserRegistrationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 from profiles.models import OCDActionUser
 from challenges.views import delete_challenges
@@ -50,6 +51,7 @@ def delete_user(request):
     delete_challenges(request.user)
 
     user = OCDActionUser.objects.get(id=request.user.id)
+    logout(request)
     user.delete()
 
     return render(request, 'profiles/my_account_confirm.html', {'deleted_user': True})
